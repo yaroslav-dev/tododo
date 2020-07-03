33e40878307648830ba6e4d867efb154e90dc58b
Yaroslav Tuvarzhiiev <yaroslav.tuvarzhiiev@gmail.com>
2020-07-04 02:38:22 +0300

    d

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# если вы публикуете по адресу https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:yaroslav-dev/tododo.git master:gh-pages
 
 cd -
