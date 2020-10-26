07a3416290ceaf1a7b4795778a87b61117b0382e
Yaroslav Tuvarzhiiev <yaroslav.tuvarzhiiev@gmail.com>
2020-10-26 18:31:58 +0200

    deploy

---
+++ precache-manifest.22954c1f00fab67c057ffe287da6a2b4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f80ddaab5ebd3217327",
+    "url": "/tododo/css/app.1da67362.css"
+  },
+  {
+    "revision": "fd7bce292918fa71c7da",
+    "url": "/tododo/css/chunk-15c3505c.e7ab5600.css"
+  },
+  {
+    "revision": "e32c3b7b88d5bbe4fed5",
+    "url": "/tododo/css/chunk-5705b360.24dd61dd.css"
+  },
+  {
+    "revision": "0a1cfb670becb80de16c",
+    "url": "/tododo/css/chunk-74f56b84.ccad38c6.css"
+  },
+  {
+    "revision": "d50ee3295bb21015523c",
+    "url": "/tododo/css/chunk-be006a86.9a25a90b.css"
+  },
+  {
+    "revision": "609f015b1fd14dc17e2a",
+    "url": "/tododo/css/chunk-c58bbbca.53d24f71.css"
+  },
+  {
+    "revision": "09922ae69c7023254233",
+    "url": "/tododo/css/chunk-vendors.856d693b.css"
+  },
+  {
+    "revision": "a2096f43f009f47d769922691d7512f6",
+    "url": "/tododo/index.html"
+  },
+  {
+    "revision": "7f80ddaab5ebd3217327",
+    "url": "/tododo/js/app.399c12d4.js"
+  },
+  {
+    "revision": "fd7bce292918fa71c7da",
+    "url": "/tododo/js/chunk-15c3505c.dfe50287.js"
+  },
+  {
+    "revision": "e32c3b7b88d5bbe4fed5",
+    "url": "/tododo/js/chunk-5705b360.653937dd.js"
+  },
+  {
+    "revision": "0a1cfb670becb80de16c",
+    "url": "/tododo/js/chunk-74f56b84.b3001b15.js"
+  },
+  {
+    "revision": "d50ee3295bb21015523c",
+    "url": "/tododo/js/chunk-be006a86.04f55102.js"
+  },
+  {
+    "revision": "609f015b1fd14dc17e2a",
+    "url": "/tododo/js/chunk-c58bbbca.4633a8f5.js"
+  },
+  {
+    "revision": "09922ae69c7023254233",
+    "url": "/tododo/js/chunk-vendors.b197c283.js"
+  },
+  {
+    "revision": "130cd047714fd2e4a2edcfc52096209b",
+    "url": "/tododo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tododo/robots.txt"
+  }
+]);
